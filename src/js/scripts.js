1fe0d92ed70c753294263fdc90ec944cfa43c723
Den <golodkov.den@gmail.com>
2020-11-20 19:53:03 +0300

    add catalog section and js tabs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,35 @@
-const slider = tns({
-    container: '.slider__inner',
-    items: 1,
-    slideBy: 'page',
-    controls: false,
-    nav: false,
-    autoplay: false
-  });
+document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
+	const slider = tns({
+		container: '.slider__inner',
+		items: 1,
+		slideBy: 'page',
+		controls: false,
+		nav: false,
+		autoplay: false
+  	});
 
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');  
-  });
+	document.querySelector('.prev').addEventListener('click', function () {
+		slider.goTo('prev');  
+  	});
 
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');  
-  });
-
+	document.querySelector('.next').addEventListener('click', function () {
+		slider.goTo('next');  
+	}); 
+	let wrapItem = document.querySelectorAll('.catalog__wrapper');
+	document.querySelectorAll('.catalog__tab-item').forEach((item, index, arr) => {
+		item.addEventListener('click', function(){
+			let tabItem = index;
+			//удаляем активный класс со всех элементов
+			arr.forEach((item, index) => {
+				item.classList.remove('catalog__tab-item_active');
+			});
+			// добавляем активный класс при клике
+			item.classList.add('catalog__tab-item_active')
+			// 
+			wrapItem.forEach((item, index) => {
+				item.classList.remove('catalog__wrapper_active');
+			});
+			wrapItem[tabItem].classList.add('catalog__wrapper_active');
+		});
+	});
+});
